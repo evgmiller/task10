05ec597f5835b47ceea62a38025908ac3f2a36a2
Anna <nusiksuper@gmail.com>
2022-12-05 15:20:55 +0300

    решение задачи №10

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Write("Введите число: ");
+int number =  int.Parse(Console.ReadLine()!);
+string Number= Convert.ToString(number);
+int [] array= new int[Number.Length];
+int i;
+for (i=0; i<Number.Length; i++)
+{
+    array[i]= Convert.ToInt32(Number[i].ToString());
+}
+Console.Write(array[1]);
